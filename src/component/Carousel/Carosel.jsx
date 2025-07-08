0e1b794c0112fb37c316ba025a56c46199fefc8e
Rahul Chaudhary <145273561+Rahulchaudharyji2@users.noreply.github.com>
2025-07-08 16:19:29 +0530

    add ui and carousel

---
+++ src/component/Carousel/Carosel.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+function Carosel() {
+  return (
+    <>
+      <div>
+        <img
+          src="https://img.freepik.com/premium-photo/colorful-eyecatching-social-media-post-template-burger-restaurant-ai-photo_1192063-13569.jpg?semt=ais_hybrid&w=740"
+          alt=""
+          srcset=""
+          style={{margin:"10px", width: "97vw" ,height:"67vh",marginLeft:"15px" ,borderRadius:"12px",boxShadow:"0 14px 15px rgba(197, 45, 45, 0.3)"}}
+        />
+      </div>
+    </>
+  );
+}
+
+export default Carosel;
